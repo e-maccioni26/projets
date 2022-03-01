94ceb5b9f00fd95cdec7e28c4dfa6f3785e83328
Elone Maccioni <100712191+e-maccioni26@users.noreply.github.com>
2022-03-01 19:24:03 +0100

    Add files via upload

---
+++ Mini-jeu/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="http://localhost:8888/COURSPHP/" method="post"></form>
+	<div>
+		<label for="name">Pseudo</label>
+		<input type="text" id="name" name="user_name">
+	</div>
+	<label for="name">Commentaire</label>
+	<input type="text" id="name" name="user_name">
+
+	<button name="button">Envoyez</button>
+
+
+
+
+
+</body>
+</html>
